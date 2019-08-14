e8681a45febe3905abfd1db8ba333668db8f90c7
Filipe Assad <3647@ucdb.br>
2019-08-14 15:57:46 -0400

    Adicionando o alerte dos termos.

--- src/pages/listagem-faturas-negociacao/listagem-faturas-negociacao.ts
+++ src/pages/listagem-faturas-negociacao/listagem-faturas-negociacao.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController, AlertController } from 'ionic-angular';
 import { Negociacao } from '../../models/negociacao.model';
 import { NegociacaoService } from '../../providers/negociacao.service';
 import { Autorizacao } from '../../models/autorizacao.model';
@@ -18,12 +18,14 @@ export class ListagemFaturasNegociacaoPage {
     private negociacaoSelecionada: Negociacao;
     private desabilitaAceitacao: boolean = true;
 
+    private texto: string = 'Por intermédio do presente isntrumento particular, <NomeBeneficiario>, brasileiro(a), <EstadoCivil>, portador(a) do RG nº <RGBeneficiario>, <OrgãoEmissor>, cadastrado no CPF sob o nº <CPFBeneficiario>, capaz, residente e domiciliado na <Logradouro>, <Numero>, bairro <Bairro>, cidade <Cidade>, Estado <Estado>, CEP <Cep>, solicito o parcelamento do débito existente em favor da CAIXA DE ASSISTENCIA DOS SERVIDORES DO ESTADO DE MATO GROSSO DO SUL - CASSEMS, na quantia total de R$ <Valor Total> () referente à receita de Contribuição Agregado Básico a ser divido em <Parcelas> parcela(s) mensais consecutivas.\n\nEstou ciente de que sendo autorizado e referido parcelamento deverei manter meu endereço atualizado para o recebimento dos boletos bancários, assumindo toda a responsabilidade pela inexatidão do mesmo, e caso não receba os boletos até o dia do vencimento, estou ciente de que devo retirar a 2ª via junto ao setor competente na CASSEMS. Para o caso de descumprimento, valor atrasado será acrescido de multa de 2%, atualização monetária pelo indice IGP-M e juros legais no importe de 1% ao mês, bem como honorários advocatícios de cobrança, fixados em 10% do total devido.\n\nCampo Grande, <Data Atual>';
 
     constructor(public navCtrl: NavController,
                 public navParams: NavParams,
                 private negociacaoService: NegociacaoService,
                 private loadingCtrl: LoadingController,
-                private negociacaoItemEstaticoService: NegociacaoItemEstaticoService) {
+                private negociacaoItemEstaticoService: NegociacaoItemEstaticoService,
+                private alertCtrl: AlertController) {
         
         this.negociacaoSelecionada = this.navParams.get('negociacaoSelecionada');
         this.itensNegociacao = this.negociacaoSelecionada.tbSolNegociacaoItems;
@@ -61,6 +63,28 @@ export class ListagemFaturasNegociacaoPage {
         }, () => {
             loader.dismiss();
         });
+    }
+
+    mostrarTermosAceitacao(){
+        let confirm = this.alertCtrl.create({
+            title: 'SOLICITAÇÃO DE PARCELAMENTO DE DÍVIDA',
+            message: this.texto,
+            buttons: [
+                {
+                    text: 'Cancelar',
+                    handler: () => {
+                        confirm.dismiss();                       
+                    }
+                },
+                {
+                    text: 'Aceitar',
+                    handler: () => {
+                        this.aceitarTermos();
+                    }
+                }
+            ]
+        });
+        confirm.present();
     }
 
     liberarAceitacao(){
